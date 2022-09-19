ac679b4837d79115508fdff3adb79faa2e75c6f2
pwierdak <paulinawierdak95@gmail.com>
2022-09-19 21:08:47 +0200

    added new class Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = " Paulina";
+        String surname = " Wierdak ";
+        int age = 26;
+        System.out.println("Hello" +name +surname +age);
+        int x = 2;
+        int y = 3;
+        int z = x+y;
+        System.out.println(z);
     }
 }
+
+
